fa68fbfe025a2eaeddf0fb291821290d8d42b146
Michel Hunziker <info@michelhunziker.com>
2020-05-21 14:18:21 +0200

    Initial commit :)

---
+++ src/Report/GitlabReport.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2020, Michel Hunziker <info@michelhunziker.com>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD-3-Clause License
+ */
+
+declare(strict_types=1);
+
+namespace Micheh\PhpCodeSniffer\Report;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Reports\Report;
+
+use function md5;
+use function str_replace;
+
+use const PHP_EOL;
+
+class GitlabReport implements Report
+{
+    /**
+     * @inheritDoc
+     */
+    public function generateFileReport($report, File $phpcsFile, $showSources = false, $width = 80): bool
+    {
+        $hasOutput = false;
+
+        foreach ($report['messages'] as $line => $lineErrors) {
+            foreach ($lineErrors as $column => $colErrors) {
+                foreach ($colErrors as $error) {
+                    $issue = [
+                        'type' => 'issue',
+                        'categories' => ['Style'],
+                        'check_name' => $error['source'],
+                        'fingerprint' => md5($report['filename'] . $error['message'] . $line),
+                        'description' => str_replace(["\n", "\r", "\t"], ['\n', '\r', '\t'], $error['message']),
+                        'location' => [
+                            'path' => $report['filename'],
+                            'lines' => [
+                                'begin' => $line,
+                                'end' => $line,
+                            ]
+                        ],
+                    ];
+
+                    if ($hasOutput) {
+                        echo ',';
+                    }
+
+                    echo json_encode($issue, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+                    $hasOutput = true;
+                }
+            }
+        }
+
+        return $hasOutput;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function generate(
+        $cachedData,
+        $totalFiles,
+        $totalErrors,
+        $totalWarnings,
+        $totalFixable,
+        $showSources = false,
+        $width = 80,
+        $interactive = false,
+        $toScreen = true
+    ): void {
+        echo '[' . $cachedData . ']' . PHP_EOL;
+    }
+}
